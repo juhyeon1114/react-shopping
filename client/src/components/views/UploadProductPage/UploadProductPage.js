9437edc0618485379de2681a16dea19d5fa5d099
unknown <juhyeon1114@gmail.com>
2020-10-26 19:56:19 +0900

    temp

---
+++ client/src/components/views/UploadProductPage/UploadProductPage.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const UploadProductionPage = () => {
+    return (
+        <div>
+            upload product page
+        </div>
+    )
+}
+
+export default UploadProductionPage;
